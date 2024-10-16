8e62b64bdbdf0e648595e8588117607030d7ed28
AH-Miller <milleralec2000@gmail.com>
2024-10-16 12:54:21 +1100

    Completed pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -13,6 +13,26 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## in the 'id' vector (ignoring NA values)
   ## NOTE: Do not round the result!
   
+  measurements <- NULL
   
-  
+  for (i in id) {
+    if (i < 10) {
+      file <- paste0(directory, "/00", as.character(i), ".csv")
+    }
+    else if (i < 100) {
+      file <- paste0(directory, "/0", as.character(i), ".csv")
+    }
+    else {
+      file <- paste0(directory, "/", as.character(i), ".csv")
+    }
+    data <- read.csv(file)
+    
+    if (pollutant == "sulfate") {
+      measurements <- c(measurements, data$"sulfate")
+    }
+    else if (pollutant == "nitrate") {
+      measurements <- c(measurements, data$"nitrate")
+    }
+  }
+  mean(measurements, na.rm = TRUE)    
 }
