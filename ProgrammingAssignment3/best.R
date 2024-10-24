96f1f0a004ede2b839b9b9beac82527bbf735a30
AH-Miller <milleralec2000@gmail.com>
2024-10-24 12:59:08 +1100

    Added assignment 3 files.

---
+++ ProgrammingAssignment3/best.R
@@ -0,0 +1,27 @@
+best <- function(state, outcome) {
+  ## Read outcome data
+  data <- read.csv("outcome-of-care-measures.csv",
+                              colClasses = "character")
+  
+  ## Check that state and outcome are valid
+  if(!(state %in% data[,7])) {
+    stop("invalid state", call. = FALSE)
+  }
+  if(!(outcome %in% c("pneumonia", "heart attack", "heart failure"))) {
+    stop("invalid outcome", call. = FALSE)
+  }
+  
+  ## Return hospital name in that state with lowest 30-day death rate
+  state_data <- data[data[,7] == state,]
+  if (outcome == 'pneumonia') {
+    min_idx <- suppressWarnings(which.min(state_data[, 11]))
+  }
+  else if (outcome == 'heart attack') {
+    min_idx <- suppressWarnings(which.min(state_data[, 17]))
+  }
+  else {
+    min_idx <- suppressWarnings(which.min(state_data[, 23]))
+  }
+  
+  state_data[min_idx, 2]
+}
